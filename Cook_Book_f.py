c869c86535cf4fc1aace77e71abb575630b9b6df
dimach110 <dima110@mail.ru>
2022-04-25 11:36:17 +0300

    Поправил файл Cook_Book_f.py - привёл в порядок элементы задания 1 (чтение из txt) и вынес в отдельную функцию + добавил функцию по записи в файл изменений (по вызову функции - не проверял пока). Добавил файл CookBook.txt из которого происходит чтение.

--- Cook_Book_f.py
+++ Cook_Book_f.py
@@ -2,10 +2,6 @@ def file_write(file_name: str, mode: str, data=''):
   with open(file_name, mode) as file:
     file.write(f"{data}\n")
 
-def file_save(file_name: str, mode: str='w', data=''):
-  with open(file_name, mode) as file:
-    file.write(data)
-
 def isdig(data):
   try:
     int(data)
@@ -13,50 +9,7 @@ def isdig(data):
   except ValueError:
     return False
 
-# cook_book = {
-#   'Каша овсяная': [
-#     {'ingredient_name': 'Овсяные хлопья', 'quantity': 3, 'measure': 'ст.л.'},
-#     {'ingredient_name': 'Молоко', 'quantity': 50, 'measure': 'мл.'},
-#     {'ingredient_name': 'Сахар', 'quantity': 2, 'measure': 'ч.л.'}
-#     ],
-#   'Суп куриный': [
-#     {'ingredient_name': 'Куриное бедро', 'quantity': 1, 'measure': 'шт.'},
-#     {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л.'},
-#     {'ingredient_name': 'Лук', 'quantity': 1, 'measure': 'шт.'},
-#     {'ingredient_name': 'Морковь', 'quantity': 1, 'measure': 'шт.'},
-#     {'ingredient_name': 'Картофель', 'quantity': 2, 'measure': 'шт.'},
-#     {'ingredient_name': 'Соль', 'quantity': 1, 'measure': 'ч.л.'}
-#     ],
-#   'Запеченный картофель': [
-#     {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг.'},
-#     {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч.'},
-#     {'ingredient_name': 'Сыр', 'quantity': 100, 'measure': 'г.'},
-#     ],
-#   'Салат': [
-#     {'ingredient_name': 'Картофель', 'quantity': 3, 'measure': 'шт.'},
-#     {'ingredient_name': 'Морковь', 'quantity': 2, 'measure': 'л.'},
-#     {'ingredient_name': 'Лук', 'quantity': 1, 'measure': 'шт.'},
-#     {'ingredient_name': 'Колбаса', 'quantity': 300, 'measure': 'гр.'},
-#     {'ingredient_name': 'Горошек', 'quantity': 1, 'measure': 'банка'},
-#     {'ingredient_name': 'Огурец', 'quantity': 1, 'measure': 'шт.'},
-#     {'ingredient_name': 'Яйцо', 'quantity': 3, 'measure': 'шт.'},
-#     {'ingredient_name': 'Майонез', 'quantity': 3, 'measure': 'ст.л.'},
-#     {'ingredient_name': 'Соль', 'quantity': 1, 'measure': 'ч.л.'}
-#     ]
-#   }
-#
-def file_save(name_file)
-  for name_food, ingred_list in cook_book.items():
-    text = f'\n{name_food} \n{len(ingred_list)}'
-    file_write(name_file, 'a', text)
-    for ingred_food in ingred_list:
-      text = f"{ingred_food.get('ingredient_name')} | {ingred_food.get('quantity')} | {ingred_food.get('measure')}"
-      file_write(name_file, 'a', text)
-
-# with open('CookBook_save.txt', "w") as file:
-#   file.write(f'{cook_book}')
-
-def read_file(name_file):
+def file_read(name_file):
   cook_book = {}
   with open(name_file, 'r') as file:
     for line in file:
@@ -70,45 +23,52 @@ def read_file(name_file):
         ingred_dict['quantity'] = ingr_list[1]
         ingred_dict['measure'] = ingr_list[2].strip(' \n')
         cook_book[name_food].append(ingred_dict)
+  return cook_book
+
+def file_save(name_file):
+  for name_food, ingred_list in cook_book.items():
+    text = f'\n{name_food} \n{len(ingred_list)}'
+    file_write(name_file, 'a', text)
+    for ingred_food in ingred_list:
+      text = f"{ingred_food.get('ingredient_name')} | {ingred_food.get('quantity')} | {ingred_food.get('measure')}"
+      file_write(name_file, 'a', text)
+
+def list_food():
+  n_food = input("Введите название блюда: ").capitalize()
+  print(n_food)
+  n = 0
+  for i in cook_book[n_food]:
+    print(i)
+    n +=1
+  print(n)
+
+def list_food_dict():
+  for food in cook_book.keys():
+    print(food)
+
+def get_shop_list_by_dishes(dishes, person_count):
+  shop_dict = {}
+  for dish in dishes:
+    if dish in cook_book.keys():
+      for ingred_dict in cook_book[dish]:
+        if ingred_dict['ingredient_name'] not in shop_dict:
+          shop_dict[ingred_dict['ingredient_name']] = \
+            {'measure': ingred_dict['measure'], 'quantity': int(ingred_dict['quantity'])*person_count}
+        else:
+          # добавляем кол-во текущего ингридиента к кол-вy ингр в уже созданном словаре по данному продукту
+          # int(ingred_dict['quantity'])*person_count + (shop_dict[ingred_dict['ingredient_name']]['quantity'])
+          shop_dict[ingred_dict['ingredient_name']] = \
+            {'measure': ingred_dict['measure'],
+             'quantity': int(ingred_dict['quantity'])*person_count + shop_dict[ingred_dict['ingredient_name']]['quantity']}
+
+    else:
+      print(f"Запрошенное блюдо {dish} отсутствует в книге рецептов")
+  return shop_dict
 
 
-# for food, ingr in cook_book.items():
-#   print(food)
-#   for ingr_l1 in ingr:
-#     print(ingr_l1)
+cook_book = file_read('CookBook.txt')
+# list_food_dict()
+print(get_shop_list_by_dishes(['Салат', 'Суп куриный'], 3))
+# for ingr_n, q in get_shop_list_by_dishes(['Салат', 'Суп куриный'], 3).items():
+#   print(f"{ingr_n},{q}")
 
-# {'Каша овсяная': [
-#   {'ingredient_name': 'Овсяные хлопья', 'quantity': '3', 'measure': 'ст.л.'},
-#   {'ingredient_name': 'Молоко', 'quantity': '50', 'measure': 'мл.'},
-#   {'ingredient_name': 'Сахар', 'quantity': '2', 'measure': 'ч.л.'}],
-# 'Суп куриный': [
-#   {'ingredient_name': 'Куриное бедро', 'quantity': '1', 'measure': 'шт.'},
-#   {'ingredient_name': 'Вода', 'quantity': '2', 'measure': 'л.'},
-#   {'ingredient_name': 'Лук', 'quantity': '1', 'measure': 'шт.'},
-#   {'ingredient_name': 'Морковь', 'quantity': '1', 'measure': 'шт.'},
-#   {'ingredient_name': 'Картофель', 'quantity': '2', 'measure': 'шт.'},
-#   {'ingredient_name': 'Соль', 'quantity': '1', 'measure': 'ч.л.'}],
-# 'Запеченный картофель': [
-#   {'ingredient_name': 'Картофель', 'quantity': '1', 'measure': 'кг.'},
-#   {'ingredient_name': 'Чеснок', 'quantity': '3', 'measure': 'зубч.'},
-#   {'ingredient_name': 'Сыр', 'quantity': '100', 'measure': 'г.'}],
-# 'Салат': [
-#   {'ingredient_name': 'Картофель', 'quantity': '3', 'measure': 'шт.'},
-#   {'ingredient_name': 'Морковь', 'quantity': '2', 'measure': 'л.'},
-#   {'ingredient_name': 'Лук', 'quantity': '1', 'measure': 'шт.'},
-#   {'ingredient_name': 'Колбаса', 'quantity': '300', 'measure': 'гр.'},
-#   {'ingredient_name': 'Горошек', 'quantity': '1', 'measure': 'банка'},
-#   {'ingredient_name': 'Огурец', 'quantity': '1', 'measure': 'шт.'},
-#   {'ingredient_name': 'Яйцо', 'quantity': '3', 'measure': 'шт.'},
-#   {'ingredient_name': 'Майонез', 'quantity': '3', 'measure': 'ст.л.'},
-#   {'ingredient_name': 'Соль', 'quantity': '1', 'measure': 'ч.л.'}
-#   ]
-# }
-#
-n_food = "Суп куриный"
-print(n_food)
-n = 0
-for i in cook_book[n_food]:
-  print(i)
-  n +=1
-print(n)
